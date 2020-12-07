c75ebdf605932e43bec01d8e51df0ba6dd0130f0
Katelyn Patrick <Patr0142@algonquinlive.com>
2020-12-06 22:37:50 -0500

    Initial Commit

---
+++ Final_Project_PassportTests/Final_Project_PassportTests.swift
@@ -0,0 +1,34 @@
+//
+//  Final_Project_PassportTests.swift
+//  Final_Project_PassportTests
+//
+//  Created by Katelyn Patrick on 2020-12-06.
+//  Copyright © 2020 Katelyn Patrick. All rights reserved.
+//
+
+import XCTest
+@testable import Final_Project_Passport
+
+class Final_Project_PassportTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
